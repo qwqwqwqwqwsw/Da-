a94f2738ef7b300fe9727375454217ced7bbdbae
Kaan Karagüney <taha-kaan-2002@outlook.com>
2025-11-16 22:16:45 +0300

    New player controller and collectibles.

---
+++ Assets/_Main/Scripts/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public GameObject effectPrefab;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroy the collectible and instantiate an effect when the player collides with it
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(effectPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
