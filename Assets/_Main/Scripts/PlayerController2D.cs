915e29fdfd47c74bbd9d3ce4f088ef9c513b1936
Kaan Karagüney <taha-kaan-2002@outlook.com>
2025-11-20 21:36:32 +0300

    Basic 2d packages

---
+++ Assets/_Main/Scripts/PlayerController2D.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    public float speed = 4f;
+    public bool canMoveDiagonally = true;
+
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+
+    private bool _isMovingHorizontally = true;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        
+        _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (canMoveDiagonally)
+        {
+            _movement = new Vector2(horizontal, vertical);
+        
+            RotatePlayer(horizontal, vertical);
+        }
+        else
+        {
+            if (horizontal != 0)
+            {
+                _isMovingHorizontally = true;
+            }
+            else if(vertical != 0)
+            {
+                _isMovingHorizontally = false;
+            }
+
+            if (_isMovingHorizontally)
+            {
+                _movement = new Vector2(horizontal, 0);
+                RotatePlayer(horizontal, 0);
+            }
+            else
+            {
+                _movement = new Vector2(0, vertical);
+                RotatePlayer(0, vertical);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _movement * speed;
+    }
+
+    private void RotatePlayer(float x, float y)
+    {
+        if (x == 0 && y == 0) return;
+        
+        float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+        
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    
+}
